426b9ad9c1bb9bd5fca93b1e43eda053855fe300
J-Acklen <joshuaacklen@gmail.com>
2023-10-26 23:50:36 -0400

    Added Zircon blocks including ore blocks & variations. Also allowed blocks to show up in inventory properly

--- src/main/java/net/acklen/tutorialmod/TutorialMod.java
+++ src/main/java/net/acklen/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.acklen.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.acklen.tutorialmod.block.ModBlocks;
 import net.acklen.tutorialmod.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -23,6 +24,7 @@ public class TutorialMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
